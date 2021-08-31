b1092711853987e3e119551fa6881ec14fc822b2
anuj-kr-jha <anuj.kr.jha@protonmail.com>
2021-08-31 13:16:48 +0530

    1.1_BoilerPlate_urlUpdate

--- client/index.js
+++ client/index.js
@@ -3,7 +3,7 @@ const services = require("../server/protos/dummy_grpc_pb");
 
 function main() {
   const client = new services.DummyServiceClient(
-    "localhost:8080",
+    "localhost:4000",
     grpc.credentials.createInsecure()
   );
   console.log("client: hello");

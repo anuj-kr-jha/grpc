1396eb6795050af16b960c3b3f3db9f33e0a964c
Anuj Kumar Jha <85032958+anuj-kr-jha@users.noreply.github.com>
2021-09-01 10:03:25 +0530

    _p1_init

--- client/index.js
+++ client/index.js
@@ -8,33 +8,17 @@ function main() {
     grpc.credentials.createInsecure()
   );
 
-  const greetRequest = new greets.GreetRequest(); // //created protobuffs greetRequest 
+  const greetRequest = new greets.GreetRequest();
   
-  const greeting = new greets.Greeting();// //created actual greeting object as specified in schema
-  greeting.setFirstName("Anuj"); // //setting field value
+  const greeting = new greets.Greeting();
+  greeting.setFirstName("Anuj"); 
   greeting.setLastName("Jha"); 
 
-  greetRequest.setGreeting(greeting); // // set Greeting
+  greetRequest.setGreeting(greeting);
 
-  // // making RPC call for function greet() on server from client
   client.greet(greetRequest, (err, res)=>{
     if (err) return console.log({error: err});
     console.log({result: res.getResult()})
-    /* RES ::
-          array:(1) ['Hello Anuj']
-          arrayIndexOffset_:-1
-        > convertedPrimitiveFields_:{}
-          messageId_:undefined
-          pivot_:1.7976931348623157e+308
-          wrappers_:null
-        > __proto__:jspb.Message 
-                constructor:ƒ (opt_data) {\n  jspb.Message.initialize(this, opt_data, 0, -1, null, null);\n}
-                getResult:ƒ () {\n  return  (jspb.Message.getFieldWithDefault(this, 1, ""));\n}
-                serializeBinary:ƒ () {\n  var writer = new jspb.BinaryWriter();\n  proto.greet.GreetResponse.serializeBinaryToWriter(this, writer);\n  return writer.getResultBuffer();\n}
-                setResult:ƒ (value) {\n  return jspb.Message.setProto3StringField(this, 1, value);\n}
-                toObject:ƒ (opt_includeInstance) {\n  return proto.greet.GreetResponse.toObject(opt_includeInstance, this);\n}
-                __proto__:Object
-    */
     })
 } 
 

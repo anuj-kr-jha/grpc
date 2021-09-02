0df5ccaad52de092208ee98033c2ac1ff708104c
Anuj Kumar Jha <85032958+anuj-kr-jha@users.noreply.github.com>
2021-09-02 18:56:28 +0530

    serverStreaming dynamic

--- client/index.js
+++ client/index.js
@@ -6,19 +6,35 @@ const greetProtoDefination = protoLoader.loadSync(PROTO_PATH, {});
 const greetPackageDefination = grpc.loadPackageDefinition(greetProtoDefination).greet; 
   
 const client = new greetPackageDefination.GreetService('localhost:4000', grpc.credentials.createInsecure());
-const greetRequest = { greeting: {firstName: "Anuj", lastName: "Jha"}}
-client.greet(greetRequest, (error, response)=>{
-    if (error) return console.log({error});
-    console.log({result: response.result})
-})
 
-/**
- * ! loadSync optional params::
- * protoLoader.loadSync('', {
- *    keepCase: true,
- *    longs: String,
- *    enums: true,
- *    defaults: true,
- *    oneofs: true,
- * })
- */
+function callGreet(){
+    const greetRequest = { greeting: {firstName: "Anuj", lastName: "Jha"}}
+    client.greet(greetRequest, (error, response)=>{
+        if (error) return console.log({error});
+        console.log({result: response.result})
+    })
+}
+
+function callGreetManyTimes() {
+    const greetManyTimesRequest = { greeting: {firstName: "Anuj", lastName: "Jha"}}
+    const call = client.greetManyTimes(greetManyTimesRequest, () => {}); // ? store function call on a variable
+
+    // ? listen to all events on that variable
+    call.on("data", (response) => {
+      console.log({ result: response.result});
+    });
+    call.on("status", (status) => {
+        console.log({status: status.details});
+    });
+    call.on("error", (error) => {
+        console.log({error: error.details});
+    });
+    call.on("end", () => console.log("server streaming ended"));
+}
+
+function main(){
+    // callGreet(); // unary api
+    callGreetManyTimes(); // server streaming api
+}
+
+main(); 
--- server/index.js
+++ server/index.js
@@ -10,10 +10,25 @@ function greet(call, callback){
     const greetResponse = {result: `hello ${call.request.greeting.firstName} ${call.request.greeting.lastName}`}
     callback(null, greetResponse);
 } 
+
+function greetManyTimes(call, callback){
+    let count = 0, intervalId = setInterval(()=>{
+      const greetManyTimesResponse = {result: `hello ${call.request.greeting.firstName} ${call.request.greeting.lastName}`}
+      call.write(greetManyTimesResponse);
+      
+      if(++count > 9) {
+        clearInterval(intervalId);
+        call.end();
+      }
+      // callback(null, greetManyTimeResponse);
+    }, 1000) 
+} 
+
 function main(){
     const server = new grpc.Server();
     server.addService(greetPackageDefination.GreetService.service, {
-        greet: greet
+        greet: greet,
+        greetManyTimes: greetManyTimes
     })
     server.bind("127.0.0.1:4000", grpc.ServerCredentials.createInsecure());
     server.start();
@@ -22,12 +37,7 @@ function main(){
 main()
 
 /**
- * ! loadSync optional params::
- * protoLoader.loadSync('', {
- *    keepCase: true,
- *    longs: String,
- *    enums: true,
- *    defaults: true,
- *    oneofs: true,
- * })
+ * * In streaming we dont need to call "callback(null, response);"
+ * * instead we keep writing on "call" by "call.write(xyz)"
+ * * and when done we "end" it by "call.end()"
  */

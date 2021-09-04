d9abe3f2b8dd08169a4b170578138da527e3f477
Anuj Kumar Jha <85032958+anuj-kr-jha@users.noreply.github.com>
2021-09-04 14:20:07 +0530

    6_error_handling

--- client/index.js
+++ client/index.js
@@ -1,77 +1,32 @@
-const _ = { delay : ttl => new Promise(resolve => setTimeout(resolve, ttl)) };
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 
-const PROTO_PATH = require('path').join(__dirname, '..', 'protos', 'greet.proto');
-const greetProtoDefination = protoLoader.loadSync(PROTO_PATH, {});
-const greetPackageDefination = grpc.loadPackageDefinition(greetProtoDefination).greet; 
-  
-const client = new greetPackageDefination.GreetService('localhost:4000', grpc.credentials.createInsecure());
+const PROTO_PATH = require('path').join(__dirname, '..', 'protos', 'square.proto');
+const squareProtoDefination = protoLoader.loadSync(PROTO_PATH, {});
 
-function callGreet(){
-    const greetRequest = { greeting: {firstName: "Anuj", lastName: "Jha"}}
-    client.greet(greetRequest, (error, response)=>{
+const squarePackageDefination = grpc.loadPackageDefinition(squareProtoDefination).square; 
+  
+const client = new squarePackageDefination.SquareService('localhost:4000', grpc.credentials.createInsecure());
+
+function getArea(){
+    const areaRequest = { dimension: { length: process.argv[2], width: process.argv[3] }};
+    client.area('passing_Wrong_Argumwent', (error, response) => {
         if (error) return console.log({error: error.details});
-        console.log({result: response.result})
+        console.log({area: response.result});
+    })
+    // * NOTE : when we pass an invalid request argument the server will recieve empty {}, and not the actual invalid args 'passing_Wrong_Argumwent'.
+}
+function getPerimeter(){
+    const perimeterRequest = { dimension: { length: process.argv[2], width: process.argv[3] }};
+    client.perimeter(perimeterRequest, (error, response) => {
+        if (error) return console.log({error: error.details});
+        console.log({perimeter: response.result});
     })
 }
 
-function callGreetManyTimes() {
-    const greetManyTimesRequest = { greeting: {firstName: "Anuj", lastName: "Jha"}}
-    const call = client.greetManyTimes(greetManyTimesRequest, () => {}); // ? store function call on a variable
-
-    // ? listen to all events on that variable
-    call.on("data", (response) => {
-        console.log({ result: response.result});
-    });
-    call.on("status", (status) => {
-        console.log({status: status.details});
-    });
-    call.on("error", (error) => {
-        console.log({error: error.details});
-    });
-    call.on("end", () => console.log("server streaming ended"));
-}
-
-function callLongGreet(){
-    const call = client.longGreet({}, (error, response) => { // notice empty req {} since this requset will be ignored due to stream type, so this will be set later using call.write()
-        if (error) return console.log({error: error.details});
-        console.log({result: response.result})
-    });
-    // TODO : wheteher server live or not client keep streaming data on client streaming api
-    // TODO : handle this.
-    let count = 0, intervalId = setInterval(()=>{
-        const longGreetRequest = { greeting: {firstName: "Anuj", lastName: "Jha"}};
-        call.write(longGreetRequest);
-        console.log('sent message : ', count + 1 )
-        if(++count > 9) {
-            clearInterval(intervalId);
-            call.end(); // we have sent all the msgs
-        }
-    }, 1000);
-}
-
-async function callGreetEveryone() {
-    const call = client.greetEveryone({}, (error, response) =>{
-        if (error) return console.log({error: error.details});
-        console.log({result: response.result})
-    });
-    call.on('data', response => console.log(`client :> hello ${response.result}`));
-    call.on('error', error => { console.log({ error: error.details}) });
-    call.on('status', status => { console.log({ status: status.details}) });
-    call.on('end', () => { console.log('Client End ...') });
-    for(let i = 0; i < 10; i++) { 
-        call.write({ greeting: { firstName: 'Anuj', lastName: 'Jha' }}) // mathing greetEveryoneRequest frpm proto schema
-        await _.delay(1000);
-    }
-    call.end();// client finished streaming
-  
-}
 function main(){
-    // callGreet(); // unary api
-    // callGreetManyTimes(); // server streaming api  
-    // callLongGreet(); // client streaming apis 
-    callGreetEveryone(); // client streaming apis
+    getArea();
+    getPerimeter();
 }
 
 main();
--- server/index.js
+++ server/index.js
@@ -1,62 +1,39 @@
-const _ = { delay : ttl => new Promise(resolve => setTimeout(resolve, ttl)) };
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 
-const PROTO_PATH = require('path').join(__dirname, '..', 'protos', 'greet.proto');
-const greetProtoDefination = protoLoader.loadSync(PROTO_PATH, {});
+const PROTO_PATH = require('path').join(__dirname, '..', 'protos', 'square.proto');
+const squareProtoDefination = protoLoader.loadSync(PROTO_PATH, {});
 
-const greetPackageDefination = grpc.loadPackageDefinition(greetProtoDefination).greet; 
+const squarePackageDefination = grpc.loadPackageDefinition(squareProtoDefination).square; 
 
-function greet(call, callback){
-  const greetResponse = {result: `hello ${call.request.greeting.firstName} ${call.request.greeting.lastName}`}
-  callback(null, greetResponse);
-} 
-function greetManyTimes(call, callback){
-  let count = 0, intervalId = setInterval(()=>{
-    const greetManyTimesResponse = {result: `hello ${call.request.greeting.firstName} ${call.request.greeting.lastName}`}
-    call.write(greetManyTimesResponse);
-    if(++count > 9) {
-      clearInterval(intervalId);
-      call.end();
-    }
-  }, 1000) 
-} 
-function longGreet(call, callback){
-  call.on('data', request => {
-    const fullName = `hello ${request.greeting.firstName} ${request.greeting.lastName}`;
-    console.log(`hello ${fullName}`);
+function area(call, callback){
+  if(!call.request.dimension) return callback({
+    code: grpc.status.INVALID_ARGUMENT,
+    message:  `invalid request argument`
   });
-  call.on('error', error => {
-    console.log({ error })
-  });
-  call.on('status', status => {
-    console.log({ status })
-  })
-  call.on('end', () => {
-    const longGreetResponse = {result: `server :> Long Greet client streaming`}
-    callback(null, longGreetResponse);
-  })
-} 
-async function greetEveryone(call, callback){
-  call.on('data', request => console.log(`hello ${request.greeting.firstName} ${request.greeting.lastName}`));
-  call.on('error', error => { console.log({ error: error.details}) });
-  call.on('status', status => { console.log({ status: status.details}) });
-  call.on('end', () => { console.log(' Server End ...') });
+  // * NOTE : We just handle error above that will prevent or grpc server from crash in case of invalid args.
 
-  for(let i = 0; i < 10; i++) { 
-    call.write({result: `Anuj Jha!`}); // mathing greetEveryoneResponse frpm proto schema
-    await _.delay(1000);
-  }
-  call.end(); // server finished streaming
-}
- 
+  const length = call.request.dimension.length;
+  const width = call.request.dimension.width;
+  callback(null, areaResponse);
+} 
+
+function perimeter(call, callback){
+  if(!call.request.dimension) return callback({
+    code: grpc.status.INVALID_ARGUMENT,
+    message:  `invalid request argument`
+  });
+  const length = call.request.dimension.length;
+  const width = call.request.dimension.width;
+  const perimeterResponse = { result: 2 * ( length + width ) }
+  callback(null, perimeterResponse);
+} 
+
 function main(){
   const server = new grpc.Server();
-  server.addService(greetPackageDefination.GreetService.service, {
-      greet: greet,
-      greetManyTimes: greetManyTimes,
-      longGreet: longGreet,
-      greetEveryone: greetEveryone
+  server.addService(squarePackageDefination.SquareService.service, {
+      area: area,
+      perimeter: perimeter
   })
   server.bindAsync("127.0.0.1:4000", grpc.ServerCredentials.createInsecure(), () => {
     server.start();
